6fc37bce0195880e80b7a2e728fed89edd610b6e
FernandoNeves <nando1997.neves@gmail.com>
2018-08-07 13:19:43 -0300

    refactoring downstream_cotrol and optimize

--- source/downstream_control.h
+++ source/downstream_control.h
@@ -6,178 +6,173 @@
 #include "logger.h"
 
 namespace protocol {
-    namespace tcp {
-        class downstream_control;
-        typedef boost::shared_ptr<downstream_control> downstream_control_ptr;
+	namespace tcp {
+		class downstream_control;
+		typedef boost::shared_ptr<downstream_control> downstream_control_ptr;
 
-        class downstream_control : public boost::enable_shared_from_this<downstream_control> {
-        public:
-            downstream_control(
-                    tcp_socket_ptr _downstream_socket
-            ) {
-                current_socket = _downstream_socket;
+		class downstream_control : public boost::enable_shared_from_this<downstream_control> {
+		public:
+			downstream_control(
+				tcp_socket_ptr _downstream_socket
+			) {
+				current_socket = _downstream_socket;
 
-                terminated = false;
-                is_recving = false;
-                is_sending = false;
-            }
+				terminated = false;
+				is_recving = false;
+				is_sending = false;
+			}
 
-            void initialize(
-            ) {
-                this->set_recv();
-            }
+			void initialize(
+			) {
+				this->set_recv();
+			}
 
-            void set_recv(
-            ) {
-                if (!current_socket)
-                    terminate();
+			void set_recv(
+			) {
+				if (!current_socket)
+					terminate();
 
-                if (terminated)
-                    return;
+				if (terminated)
+					return;
 
-                if (is_recving)
-                    return;
+				if (is_recving)
+					return;
 
-                is_recving = true;
+				is_recving = true;
 
-                buffer_ptr data_buffer =
-                        boost::make_shared<buffer_t>();
+				auto boost_buffer =
+					boost::asio::buffer(
+						buffer_recv,
+						sizeof(buffer_recv)
+					);
 
-                /* TODO - MAYBE BUG, WHY buffer is not large ENOUGH */
-                data_buffer->resize(UINT16_MAX);
+				auto bounded_function = boost::bind(
+					&downstream_control::on_recv_packet,
+					this,
+					this->shared_from_this(),
+					boost::asio::placeholders::error,
+					boost::asio::placeholders::bytes_transferred
+				);
 
-                auto boost_buffer =
-                        boost::asio::buffer(
-                                (char*)data_buffer->data(),
-                                data_buffer->size()
-                        );
+				current_socket->async_receive(
+					boost_buffer,
+					bounded_function
+				);
+			}
 
-                auto bounded_function = boost::bind(
-                        &downstream_control::on_recv_packet,
-                        this,
-                        this->shared_from_this(),
-                        data_buffer,
-                        boost::asio::placeholders::error,
-                        boost::asio::placeholders::bytes_transferred
-                );
+			void check_send(
+			) {
+				if (!current_socket)
+					terminate();
 
-                current_socket->async_receive(
-                        boost_buffer,
-                        bounded_function
-                );
-            }
+				if (terminated)
+					return;
 
-            void check_send(
-            ) {
-                if (!current_socket)
-                    terminate();
+				if (is_sending)
+					return;
 
-                if (terminated)
-                    return;
+				buffer_ptr data_buffer =
+					this->get_data_buffer();
 
-                if (is_sending)
-                    return;
+				if (!data_buffer)
+					return;
 
-                buffer_ptr data_buffer =
-                        this->get_data_buffer();
+				is_sending = true;
 
-                if (!data_buffer)
-                    return;
+				auto boost_buffer =
+					boost::asio::buffer(
+					(char*)data_buffer->data(),
+						data_buffer->size()
+					);
 
-                is_sending = true;
+				auto bounded_function = boost::bind(
+					&downstream_control::on_send_packet,
+					this,
+					this->shared_from_this(),
+					data_buffer,
+					boost::asio::placeholders::error,
+					boost::asio::placeholders::bytes_transferred
+				);
 
-                auto boost_buffer =
-                        boost::asio::buffer(
-                                (char*)data_buffer->data(),
-                                data_buffer->size()
-                        );
+				current_socket->async_send(
+					boost_buffer,
+					bounded_function
+				);
+			}
 
-                auto bounded_function = boost::bind(
-                        &downstream_control::on_send_packet,
-                        this,
-                        this->shared_from_this(),
-                        data_buffer,
-                        boost::asio::placeholders::error,
-                        boost::asio::placeholders::bytes_transferred
-                );
+			void on_recv_packet(
+				downstream_control_ptr this_holder,
+				const boost_error& error_code,
+				const size_t& bytes_transferred
+			) {
+				is_recving = false;
 
-                current_socket->async_send(
-                        boost_buffer,
-                        bounded_function
-                );
-            }
+				if (error_code) {
+					this->terminate();
+					return;
+				}
 
-            void on_recv_packet(
-                    downstream_control_ptr this_holder,
-                    buffer_ptr data_buffer,
-                    const boost_error& error_code,
-                    const size_t& bytes_transferred
-            ) {
-                is_recving = false;
+				buffer_ptr data_buffer =
+					boost::make_shared<buffer_t>(
+						buffer_recv,
+						bytes_transferred
+						);
 
-                if (error_code) {
-                    this->terminate();
-                    return;
-                }
+				this->add_data_buffer(data_buffer);
 
-                data_buffer->resize(bytes_transferred);
-                this->add_data_buffer(data_buffer);
+				this->check_send();
+				this->set_recv();
+			}
 
-                helper::logger::get() << "\n " << __FUNCTION__ << ": " << __LINE__ << " - RECV_BYTES: " << *data_buffer;
+			void on_send_packet(
+				downstream_control_ptr this_holder,
+				buffer_ptr data_buffer,
+				const boost_error& error_code,
+				const size_t& bytes_transferred
+			) {
+				is_sending = false;
 
-                this->check_send();
-                this->set_recv();
-            }
+				if (error_code) {
+					this->terminate();
+					return;
+				}
 
-            void on_send_packet(
-                    downstream_control_ptr this_holder,
-                    buffer_ptr data_buffer,
-                    const boost_error& error_code,
-                    const size_t& bytes_transferred
-            ) {
-                is_sending = false;
+				this->check_send();
+				this->set_recv();
+			}
 
-                if (error_code) {
-                    this->terminate();
-                    return;
-                }
+			void add_data_buffer(
+				buffer_ptr data_buffer
+			) {
+				this->to_downstream_buffer.push_back(data_buffer);
+				this->check_send();
+			}
 
-                this->check_send();
-                this->set_recv();
-            }
+			buffer_ptr get_data_buffer(
+			) {
+				if (terminated)
+					return nullptr;
 
-            void add_data_buffer(
-                    buffer_ptr data_buffer
-            ) {
-                this->to_downstream_buffer.push_back(data_buffer);
-                this->check_send();
-            }
+				if (!to_downstream_buffer.size())
+					return nullptr;
 
-            buffer_ptr get_data_buffer(
-            ) {
-                if (terminated)
-                    return nullptr;
+				auto data_buffer = *to_downstream_buffer.begin();
+				to_downstream_buffer.erase(to_downstream_buffer.begin());
 
-                if (!to_downstream_buffer.size())
-                    return nullptr;
+				return data_buffer;
+			}
 
-                auto data_buffer = *to_downstream_buffer.begin();
-                to_downstream_buffer.erase(to_downstream_buffer.begin());
+			void terminate() { terminated = true; }
+		private:
+			char buffer_recv[100 * 1000];
 
-                return data_buffer;
-            }
+			bool terminated;
+			bool is_recving;
+			bool is_sending;
 
-            void terminate() { terminated = true; }
-        private:
-            char buffer_recv[100 * 1000];
-
-            bool terminated;
-            bool is_recving;
-            bool is_sending;
-
-            tcp_socket_ptr current_socket;
-            std::vector<buffer_ptr> to_downstream_buffer;
-        };
-    }
+			tcp_socket_ptr current_socket;
+			std::vector<buffer_ptr> to_downstream_buffer;
+		};
+	}
 }
 #endif // !DOWNSTREAM_CONTROL_H
