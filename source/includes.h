054d75fe93d630140ffacc38b3b39c6f25aebf46
FernandoNeves <nando1997.neves@gmail.com>
2018-08-09 17:17:46 -0300

    remove json and logger

--- source/includes.h
+++ source/includes.h
@@ -13,6 +13,9 @@
 #include <stdint.h>
 #include <iostream>
 
-#define DEFAULT_BUFFER_SIZE 100 * 1000
+#define DEFAULT_BUFFER_SIZE		100 * 1000
+
+#define DEFAULT_PORT_TCP		9090
+#define DEFAULT_PORT_UDP		9191
 
 #endif // !INCLUDES_H
--- source/settings.h
+++ source/settings.h
@@ -7,34 +7,14 @@
 
 #include "logger.h"
 
-/* JSON LIB INCLUDES */
-//#include <lib_json/json.h>
-
 namespace helper {
 	class settings {
 	public:
 		void load_settings(
 			std::string file_name
 		) {
-			/* auto json_value = Json::load_json_file(file_name);
-
-			 if (json_value.isNull() || json_value.empty())
-				 return;
-
-			 std::string tcp_address = json_value["tcp_address"].asString();
-			 uint16_t tcp_port = json_value["tcp_port"].asUInt();
-			 uint32_t max_file_size = json_value["file_max_size"].asUInt();
-
-			 logger::get();
-			 logger::get().set_max_file_size(max_file_size);
-
-			 protocol::tcp::tcp_local_server::get()->set_local_endpoint(
-					 tcp_address,
-					 tcp_port
-			 );*/
-
-			protocol::tcp::tcp_local_server::get()->set_local_endpoint("0.0.0.0", 9090);
-			protocol::udp::udp_local_server::get()->set_local_endpoint("0.0.0.0", 9191);
+			protocol::tcp::tcp_local_server::get()->set_local_endpoint("0.0.0.0", DEFAULT_PORT_TCP);
+			protocol::udp::udp_local_server::get()->set_local_endpoint("0.0.0.0", DEFAULT_PORT_UDP);
 		}
 
 		static settings* get() {
